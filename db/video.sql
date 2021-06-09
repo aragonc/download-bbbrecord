95fbc50bab70cd963cb6f6b92d291836cf2b845a
Jian Camposano <jiancamposano@gmail.com>
2021-06-08 20:43:23 -0500

    BBB Record!

--- db/video.sql
+++ db/video.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 24-05-2021 a las 18:13:05
+-- Tiempo de generación: 08-06-2021 a las 21:42:06
 -- Versión del servidor: 8.0.25-0ubuntu0.20.04.1
 -- Versión de PHP: 7.2.34-21+ubuntu20.04.1+deb.sury.org+1
 
@@ -25,25 +25,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `categoria`
+-- Estructura de tabla para la tabla `category`
 --
 
-CREATE TABLE `categoria` (
-  `idCat` int NOT NULL,
-  `nombreCat` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+CREATE TABLE `category` (
+  `idCategory` int NOT NULL,
+  `nameCategory` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
--- Volcado de datos para la tabla `categoria`
+-- Volcado de datos para la tabla `category`
 --
 
-INSERT INTO `categoria` (`idCat`, `nombreCat`) VALUES
-(1, 'cumbia'),
-(3, 'rock'),
-(4, 'poplo'),
-(7, 'lorooop'),
-(18, 'merengue'),
-(19, 'bailable');
+INSERT INTO `category` (`idCategory`, `nameCategory`) VALUES
+(1, 'cumbiaaaaafhyg'),
+(26, 'electro'),
+(27, 'merengue');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `iduser` int NOT NULL,
+  `user` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`iduser`, `user`, `password`) VALUES
+(1, 'admin', 'admin'),
+(2, 'jian', 'jian');
 
 -- --------------------------------------------------------
 
@@ -52,55 +69,67 @@ INSERT INTO `categoria` (`idCat`, `nombreCat`) VALUES
 --
 
 CREATE TABLE `videos` (
-  `id` int NOT NULL,
+  `idVideo` int NOT NULL,
   `url` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `idCat` int NOT NULL,
-  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `duracion` decimal(10,2) NOT NULL
+  `idCategory` int NOT NULL,
+  `nameVideo` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `minutesVideo` int NOT NULL,
+  `secondsVideo` int NOT NULL,
+  `date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `videos`
 --
 
-INSERT INTO `videos` (`id`, `url`, `idCat`, `nombre`, `duracion`) VALUES
-(19, 'https://www.youtube.com/watch?v=5KkY9ktaAlU', 1, '89ui', '4.35'),
-(21, 'https://www.youtube.com/watch?v=o1PbzfzbmTM', 1, 'agua marina', '3.23'),
-(22, 'https://www.youtube.com/watch?v=o1PbzfzbmTM', 1, 'hotel transilvaniadd', '4.58'),
-(26, 'https://www.youtube.com/watch?v=o1PbzfzbmTM', 3, 'ultimos del 2021', '15.25');
+INSERT INTO `videos` (`idVideo`, `url`, `idCategory`, `nameVideo`, `minutesVideo`, `secondsVideo`, `date`) VALUES
+(44, 'https://', 1, 'agua marina oficial 202', 1, 14, '2021-06-04'),
+(48, 'https://www.youtube.com/watch?v=o1PbzfzbmTM', 1, 'hotel transilvania', 1, 1, '2021-06-11');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `categoria`
+-- Indices de la tabla `category`
 --
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`idCat`);
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`idCategory`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`iduser`);
 
 --
 -- Indices de la tabla `videos`
 --
 ALTER TABLE `videos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idCat` (`idCat`);
+  ADD PRIMARY KEY (`idVideo`),
+  ADD KEY `idCat` (`idCategory`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `categoria`
+-- AUTO_INCREMENT de la tabla `category`
 --
-ALTER TABLE `categoria`
-  MODIFY `idCat` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+ALTER TABLE `category`
+  MODIFY `idCategory` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `iduser` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `videos`
 --
 ALTER TABLE `videos`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `idVideo` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- Restricciones para tablas volcadas
@@ -110,7 +139,7 @@ ALTER TABLE `videos`
 -- Filtros para la tabla `videos`
 --
 ALTER TABLE `videos`
-  ADD CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`idCat`) REFERENCES `categoria` (`idCat`) ON DELETE RESTRICT ON UPDATE CASCADE;
+  ADD CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`idCategory`) REFERENCES `category` (`idCategory`) ON DELETE RESTRICT ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
